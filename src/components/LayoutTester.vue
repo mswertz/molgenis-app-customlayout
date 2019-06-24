f68a4a31e1d3888db2233d21784a587269521289
mswertz <m.a.swertz@rug.nl>
2019-06-24 23:20:43 +0200

    small layout tweaks

--- src/components/LayoutTester.vue
+++ src/components/LayoutTester.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
-      Choose layout
+      Layout: 
       <select v-model="layout">
-        <option selected value="collections1.vue">collections1</option>
+        <option value="collections1.vue">collections1</option>
       </select>
-      <br/>
-      Layout below this line, above next line. 
-      <br/>Use pager to check different records.
-      <br/>
-      <button :disabled="index < 1" v-on:click="index -= 1">Prev</button>
-      <button v-on:click="index += 1">Next</button>
-      <hr/>
+      Use pager to check different records.
+      <button type="button" class="btn btn-primary" :disabled="index < 1" v-on:click="index -= 1">Prev</button>
+      <button type="button" class="btn btn-primary" v-on:click="index += 1">Next</button>
+      <div class="card card-body">
       <ApplyLayout :layout="layout" :key="layout" :record="record"/>
-      <hr/>
+      </div>
       For debuging json state below
       <pre>{{ results }}</pre>
   </div>
@@ -25,7 +22,7 @@ import ApplyLayout from './ApplyLayout.vue'
 export default {
   data: function() {return { 
     results:[{name:"Loading ... wait a minute ..."}],
-    layout: 'error.vue',
+    layout: 'collections1.vue',
     index: 0
   }},
   components: {
@@ -37,7 +34,7 @@ export default {
     }
   },
   mounted() {
-    axios.get("https://directory.bbmri-eric.eu/api/v1/eu_bbmri_eric_collections?expand=materials,biobank,diagnosis_available,network,contact,data_categories")
+    axios.get("https://directory.bbmri-eric.eu/api/v1/eu_bbmri_eric_collections?expand=materials,biobank,diagnosis_available,network,contact,data_categories&num=10")
     .then(response => {this.results = response.data.items})
   }
 }
