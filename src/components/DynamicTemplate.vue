1d29a14ac3fd3ab18f80e4db1a92fe1451fb6c66
mswertz <m.a.swertz@rug.nl>
2019-07-04 19:23:36 +0200

    added fullscreen view option

--- src/components/DynamicTemplate.vue
+++ src/components/DynamicTemplate.vue
@@ -1,42 +1,53 @@
 <template>
  <div>
-     <div class="container-fluid">
-        <div class="card-group">
-            <div class="card">
-            <div class="card-body" >
-                <h1>MOLGENIS Entity Template Editor (ALPHA)</h1>
-                <div>This app allows you to retrieve data from MOLGENIS REST query and then edit a custom layout template to render the result.
-                    Future work will include ability to safe your template and link it to the entity in your query.
-                </div>
-                <br/>
-                <div class="form-group">
-                    <label for="url">MOLGENIS REST query (one record will be passed into template):</label>
-                    <input class="form-control" type="" v-model="url"/>
-                </div>
-            </div>
-            </div>
-        </div>
-        <div class="card-group">
+    <div v-if="fullscreen == true">
+        <button type="button" class="btn btn-primary" :disabled="index < 1" v-on:click="index -= 1">Prev</button>&nbsp;
+        <button type="button" class="btn btn-primary" v-on:click="index += 1">Next</button>&nbsp;
+        <button type="button" class="btn btn-primary" v-on:click="fullscreen=false;">Close fullscreen</button>
+        <VRuntimeTemplate :template="vue"/>
+     </div>
+    <div v-if="fullscreen == false">
+        <div class="container-fluid">
+            <div class="card-group">
                 <div class="card">
-                    <div class="card-body" >
-                        <h2>Edit template below:</h2>
-                        <PrismEditor v-model="template" language="js" line-numbers="line-numbers" style="width:100%"/>
+                <div class="card-body" >
+                    <h1>MOLGENIS Entity Template Editor (ALPHA)</h1>
+                    <div>This app allows you to retrieve data from MOLGENIS REST query and then edit a custom layout template to render the result.
+                        Future work will include ability to safe your template and link it to the entity in your query.
+                    </div>
+                    <br/>
+                    <div class="form-group">
+                        <label for="url">MOLGENIS REST query (one record will be passed into template):</label>
+                        <input class="form-control" type="" v-model="url"/>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card-body">
-                        <h2>Preview result:
-                        <button type="button" class="btn btn-primary btn-sm" :disabled="index < 1" v-on:click="index -= 1">Prev</button>
-                        <button type="button" class="btn btn-primary btn-sm" v-on:click="index += 1">Next</button>
-                        </h2>
-                        <VRuntimeTemplate :template="vue"/>
-                    </div>
                 </div>
-        </div>
-        <div class="card-group">
-            <div class="card card-body" style="width: 100%">
-                <h2>For debuging json state below</h2>
-                <pre>record = {{ record }}</pre>
+            </div>
+            <div class="card-group">
+                    <div class="card">
+                        <div class="card-body" >
+                            <h2>Edit template below:</h2>
+                            <hr/>
+                            <PrismEditor v-model="template" language="js" line-numbers="linenumbers" style="width:100%"/>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h2>Preview result below:<div class="btn-group" role="group" >
+                            <button type="button" class="btn btn-primary btn-sm" :disabled="index < 1" v-on:click="index -= 1">Prev</button>
+                            <button type="button" class="btn btn-primary btn-sm" v-on:click="index += 1">Next</button>
+                            <button type="button" class="btn btn-primary btn-sm" v-on:click="fullscreen=true;">Show fullscreen</button>
+                            </div></h2>
+                            <hr/>
+                            <VRuntimeTemplate :template="vue"/>
+                        </div>
+                    </div>
+            </div>
+            <div class="card-group">
+                <div class="card card-body" style="width: 100%">
+                    <h2>For debuging json state below</h2>
+                    <pre>record = {{ record }}</pre>
+                </div>
             </div>
         </div>
     </div>
@@ -58,7 +69,8 @@ export default {
     template: '<h1>{{record.name}}<span v-if="record.acronym">({{record.acronym}})</span></h1>\n{{record.description}}',
     results:[{name:"Loading...."}],
     index: 0, 
-    "line-numbers": true, 
+    fullscreen: false,
+    linenumbers: true, 
     url: "https://directory.bbmri-eric.eu/api/v1/eu_bbmri_eric_collections?expand=materials,biobank,diagnosis_available,network,contact,data_categories&num=10"
   }),
   components : {
